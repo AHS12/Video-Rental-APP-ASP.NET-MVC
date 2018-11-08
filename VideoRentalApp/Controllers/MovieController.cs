db3ae460e16b092a84a717960fa9d4ae6ba0a3e0
Md Azizul Hakim <ahslucky12@gmail.com>
2018-11-09 01:24:25 +0600

    some finish touch

--- VideoRentalApp/Controllers/MovieController.cs
+++ VideoRentalApp/Controllers/MovieController.cs
@@ -4,7 +4,6 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using VideoRentalApp.Managers;
 using VideoRentalApp.Models;
 using VideoRentalAPP.Models;
 using VideoRentalApp.ViewModels;
